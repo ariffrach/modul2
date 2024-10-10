5a84dea6ec4425c250d33c99cd750fd234bbfe2c
Hanahnur <161694634+Hanahnur@users.noreply.github.com>
2024-10-10 20:40:13 +0700

    Add files via upload

---
+++ 2311102312_Hanah Nur Azizah/Guided/guided3.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var celsius float64
+	fmt.Print("Masukkan suhu dalam derajat Celsius: ")
+	fmt.Scan(&celsius)
+	fahrenheit := (celsius * 9 / 5) + 32
+	reamur := celsius * 4 / 5
+	kelvin := celsius + 273.15
+	fmt.Printf("Temperatur Celsius: %.2f\n", celsius)
+	fmt.Printf("Derajat Reamur: %.2f\n", reamur)
+	fmt.Printf("Derajat Fahrenheit: %.2f\n", fahrenheit)
+	fmt.Printf("Derajat Kelvin: %.2f\n", kelvin)
+}
